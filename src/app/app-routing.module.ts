dc0bd3ae5579a5d19f8d8ba75343ec2d9bca1c4d
petern1953 <petern1953@gmail.com>
2021-03-22 18:31:11 +0100

    readme, src, json from 1234

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'hero/:id', component: HeroDetailComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Hero } from '../model/hero';
+import { HeroService } from '../service/hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.scss']
+})
+export class HeroDetailComponent implements OnInit {
+  hero: Hero = new Hero();
+
+  constructor(
+    private ar: ActivatedRoute,
+    private hService: HeroService,
+  ) {
+    this.ar.params.subscribe(params => {
+      this.hService.getOne(params.id)
+        .forEach(hero => {
+          this.hero = hero;
+        });
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
